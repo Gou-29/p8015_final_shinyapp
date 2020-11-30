e12233c2ac3d79b7e90a6432060aeff3a8c1ea38
Gou-29 <734384340@qq.com>
2020-11-30 17:04:00 +0800

    - Bug fix for the radar plot

--- global.R
+++ global.R
@@ -9,6 +9,10 @@ library(shinycssloaders)
 library(tidyverse)
 library(wordcloud2)
 
-radarplot_data <- read_csv("./Dataset/shiny_radar.csv")
-
+radarplot_data <- read_csv("shiny_radar.csv")
+name_list_radar <- c("movie_title","genres","num_critic_for_reviews",
+                     "duration","director_facebook_likes","actor_3_facebook_likes",
+                     "actor_2_facebook_likes","actor_1_facebook_likes"
+                     ,"num_voted_users", "facenumber_in_poster","num_user_for_reviews","budget"
+                     ,"imdb_score","aspect_ratio","movie_facebook_likes")
 
